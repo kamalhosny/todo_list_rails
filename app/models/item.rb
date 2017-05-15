41795186e09e374ca232aae89a64fde48890beeb
kamalhosny <kamal-aldin-hosny@hotmail.com>
2017-05-15 18:08:51 +0200

    add Devise for user and list model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
+  belongs_to :list
   validates :content ,presence:true , length: { maximum: 100 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :items, except: [:new, :edit, :show]
   # root "item#index"
 
